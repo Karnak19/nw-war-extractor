827548818c950172d3b4b34530f40602a9a7574f
Basile Vernouillet <basile64.v@gmail.com>
2022-01-27 20:18:00 +0100

    feat(auth): init auth modules

---
+++ apps/api/src/magic/magic.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { Magic } from '@magic-sdk/admin';
+
+@Injectable()
+export class MagicService extends Magic {
+  constructor() {
+    super(process.env.MAGIC_SECRET);
+  }
+}
